3de3020154286c4a34f4cbd70875635439f3ee73
LukazinDev <lucasdevctt01@gmail.com>
2025-04-05 17:48:06 -0300

    update models : atualizei para aparecer imagens

--- forum/models.py
+++ forum/models.py
@@ -16,7 +16,7 @@ from django.db import models
 class Livro(models.Model):
     titulo = models.CharField(max_length=200)
     autor = models.CharField(max_length=200)
-    imagem = models.URLField()  # ou ImageField se quiser upload
+    imagem = models.ImageField(upload_to='livros/')
     preco = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
