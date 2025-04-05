2e52c5cf292f06a787df1a51973a374d6651ac0b
JoaoGuilherme <jgbof@cesar.school>
2025-04-04 20:29:38 -0400

    Atualizar URLs do app

--- forum/urls.py
+++ forum/urls.py
@@ -1,14 +1,7 @@
 from django.urls import path
-from .views import registrar_usuario
+from .views import home, registrar_usuario
 
 urlpatterns = [
+    path('', home, name='home'),
     path('cadastro/', registrar_usuario, name='cadastro'),
-]
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),  # acessa a home quando for /
-    path('cadastro/', views.registrar_usuario, name='cadastro'),  # acessa /cadastro/
 ]
