88e67150cf278fb98e91c31bf6961bd4f857fa5d
JoaoGuilherme <jgbof@cesar.school>
2025-04-04 20:28:08 -0400

    formulário com campos extras

--- forum/forms.py
+++ forum/forms.py
@@ -1,10 +1,14 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django import forms
 
 class RegistroUsuarioForm(UserCreationForm):
+    nome_completo = forms.CharField(max_length=150, required=True)
+    cpf = forms.CharField(max_length=14, required=True)
+    endereco = forms.CharField(max_length=255, required=True)
+    telefone = forms.CharField(max_length=15, required=True)
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'nome_completo', 'cpf', 'endereco', 'telefone', 'email', 'password1', 'password2']
