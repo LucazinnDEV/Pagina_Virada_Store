eaa44b10ddef0bafa917657cedf43eeea50ba41e
Rafaellc-DEV <rafaelcosta.2600@gmail.com>
2025-04-05 17:11:13 -0300

    Ajustando o caminho da home

--- forum/urls.py
+++ forum/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.shortcuts import redirect
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', lambda request: redirect('home')),
+    path('home/', views.home, name='home'),
     path('cadastro/', views.registrar_usuario, name='cadastro'),
     path('categorias/', views.categorias, name='categorias'),
     path('mais-vendidos/', views.mais_vendidos, name='mais_vendidos'),
