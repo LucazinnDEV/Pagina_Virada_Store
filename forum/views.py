aa1d869fba86a8894fd54008d56838d273f3eb02
JoaoGuilherme <jgbof@cesar.school>
2025-04-04 21:14:42 -0400

    ' '

--- forum/views.py
+++ forum/views.py
@@ -13,5 +13,20 @@ def registrar_usuario(request):
             return redirect('home')
     else:
         form = RegistroUsuarioForm()
+<<<<<<< HEAD
     return render(request, 'cadastro.html', {'form': form})
     
+=======
+    return render(request, 'forum/cadastro.html', {'form': form})
+
+
+from django.shortcuts import render
+def home(request):
+    return render(request, 'forum/home.html')
+def categorias(request):
+    return render(request, 'forum/categorias.html')
+
+def mais_vendidos(request):
+    return render(request, 'forum/mais_vendidos.html')
+
+>>>>>>> cfe045a5180d455fe49f3cd6af0466d27d7355c7
