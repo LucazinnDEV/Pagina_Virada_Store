ee088487521897f00a32d16c6d1c21671856f3a7
JoaoGuilherme <jgbof@cesar.school>
2025-04-04 20:28:41 -0400

    view que usa o formulário

--- forum/views.py
+++ forum/views.py
@@ -6,14 +6,12 @@ def registrar_usuario(request):
     if request.method == 'POST':
         form = RegistroUsuarioForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save(commit=False)
+            user.email = form.cleaned_data.get('email')
+            user.save()
             messages.success(request, 'Cadastro realizado com sucesso!')
-            return redirect('login')  # ajuste conforme sua URL de login
+            return redirect('home')
     else:
         form = RegistroUsuarioForm()
-    return render(request, 'forum/cadastro.html', {'form': form})
-
-
-from django.shortcuts import render
-def home(request):
-    return render(request, 'forum/home.html')
+    return render(request, 'cadastro.html', {'form': form})
+    
