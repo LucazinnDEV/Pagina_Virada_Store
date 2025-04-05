201d36129fbf7fcd33f4f6132ca2db12b8185669
Mihaael <hugodiego.898@gmail.com>
2025-04-05 20:35:54 -0300

    Adicionando urls de login e logout

--- forum/urls.py
+++ forum/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from django.shortcuts import redirect
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', lambda request: redirect('home')),  # redireciona para /home
-    path('home/', views.home, name='home'),  # precisa da view home
-    path('cadastro/', views.registrar_usuario, name='cadastro'),  # já existe
-    path('categorias/', views.categorias, name='categorias'),  # precisa da view
-    path('mais-vendidos/', views.mais_vendidos, name='mais_vendidos'),  # precisa da view
+    path('', lambda request: redirect('home')),  
+    path('home/', views.home, name='home'),  
+    path('cadastro/', views.registrar_usuario, name='cadastro'),  
+    path('categorias/', views.categorias, name='categorias'),  
+    path('mais-vendidos/', views.mais_vendidos, name='mais_vendidos'),  
+    path('login/', auth_views.LoginView.as_view(template_name='forum/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
 ]
