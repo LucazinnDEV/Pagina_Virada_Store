212c672802967ede1e279051ed6586e17f63cdfb
Mihaael <hugodiego.898@gmail.com>
2025-04-05 15:01:36 -0300

    Corrigindo conflito de merge nas novas urls.py

--- forum/urls.py
+++ forum/urls.py
@@ -1,13 +1,4 @@
 from django.urls import path
-from .views import home, registrar_usuario
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('cadastro/', registrar_usuario, name='cadastro'),
-]
-<<<<<<< HEAD
-
-from django.urls import path
 from . import views
 
 urlpatterns = [
@@ -16,5 +7,3 @@ urlpatterns = [
     path('categorias/', views.categorias, name='categorias'),
     path('mais-vendidos/', views.mais_vendidos, name='mais_vendidos'),
 ]
-=======
->>>>>>> aa1d869fba86a8894fd54008d56838d273f3eb02
