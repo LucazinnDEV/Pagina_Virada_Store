b08365e1edc5b0399956e0fcb319da9d9338be35
JoaoGuilherme <jgbof@cesar.school>
2025-04-06 15:23:03 -0400

    registra Livro, Categoria e Perfil no admin

--- forum/admin.py
+++ forum/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Livro
+from .models import Livro, Categoria, Perfil
 
 admin.site.register(Livro)
+admin.site.register(Categoria)
+admin.site.register(Perfil)
