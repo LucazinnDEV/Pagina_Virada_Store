ec91f6d824a2e436b9e0c4b23f4bfba3dee9cd4c
LukazinDev <lucasdevctt01@gmail.com>
2025-04-06 13:04:49 -0300

    update de urls , so correção de erros

--- forum/urls.py
+++ forum/urls.py
@@ -1,14 +1,13 @@
+# forum/urls.py
 from django.urls import path
-from django.shortcuts import redirect
 from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', lambda request: redirect('home')),  
-    path('home/', views.home, name='home'),  
-    path('cadastro/', views.registrar_usuario, name='cadastro'),  
-    path('categorias/', views.categorias, name='categorias'),  
-    path('mais-vendidos/', views.mais_vendidos, name='mais_vendidos'),  
+    path('', views.home, name='home'),  # Mantém só essa
+    path('cadastro/', views.registrar_usuario, name='cadastro'),
+    path('categorias/', views.categorias, name='categorias'),
+    path('mais-vendidos/', views.mais_vendidos, name='mais_vendidos'),
     path('login/', auth_views.LoginView.as_view(template_name='forum/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
 ]
