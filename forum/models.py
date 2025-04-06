3795f0c1ca8d74dbf8754da8b62e7bbabe5bfd7b
Mihaael <hugodiego.898@gmail.com>
2025-04-06 20:09:12 -0300

    Validando informações da class Perfil

--- forum/models.py
+++ forum/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import FileExtensionValidator
 
 class Perfil(models.Model):
     TIPO_PESSOA_CHOICES = [
@@ -7,31 +8,33 @@ class Perfil(models.Model):
         ('J', 'Pessoa Jurídica'),
     ]
 
-    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
     nome = models.CharField(max_length=100)
     sobrenome = models.CharField(max_length=100)
     tipo_pessoa = models.CharField(max_length=1, choices=TIPO_PESSOA_CHOICES)
     data_nascimento = models.DateField()
-    cpf = models.CharField(max_length=14)
+    cpf = models.CharField(max_length=14, unique=True)
     cep = models.CharField(max_length=9)
     endereco = models.CharField(max_length=200)
-    telefone = models.CharField(max_length=15)
+    telefone = models.CharField(max_length=15, unique=True)
 
     def __str__(self):
         return f"{self.nome} {self.sobrenome}"
 
-
 class Categoria(models.Model):
     nome = models.CharField(max_length=100)
-    imagem = models.URLField(blank=True)  # Campo para imagem da categoria
+    imagem = models.URLField(blank=True)
 
     def __str__(self):
         return self.nome
 
-
 class Livro(models.Model):
     titulo = models.CharField(max_length=200)
-    imagem = models.CharField(max_length=200)
+    imagem = models.ImageField(
+        upload_to='livros/',
+        validators=[FileExtensionValidator(['jpg', 'png', 'jpeg'])],
+        blank=True, null=True
+    )
     autor = models.CharField(max_length=100)
     preco = models.DecimalField(max_digits=6, decimal_places=2)
     categoria = models.ForeignKey(
