60f8cc109d2c85e1cc5efb3d463c72f2d6680530
JoaoGuilherme <jgbof@cesar.school>
2025-04-06 15:24:11 -0400

    adiciona rotas para home, categorias, mais vendidos e autenticação

--- forum/urls.py
+++ forum/urls.py
@@ -1,13 +1,11 @@
-# forum/urls.py
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Mantém só essa
-    path('cadastro/', views.registrar_usuario, name='cadastro'),
+    path('', views.home, name='home'),
     path('categorias/', views.categorias, name='categorias'),
-    path('mais-vendidos/', views.mais_vendidos, name='mais_vendidos'),
-    path('login/', auth_views.LoginView.as_view(template_name='forum/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    path('mais_vendidos/', views.mais_vendidos, name='mais_vendidos'),
+    path('registrar/', views.registrar_usuario, name='registrar'),
+    path('login/', views.login_usuario, name='login'),
+    path('logout/', views.logout_usuario, name='logout'),
 ]
