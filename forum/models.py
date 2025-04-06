9da39597d19299c806ff87820c7883bf4ff98afe
JoaoGuilherme <jgbof@cesar.school>
2025-04-06 15:23:55 -0400

    adiciona métodos __str__ aos modelos Perfil, Categoria e Livro

--- forum/models.py
+++ forum/models.py
@@ -17,15 +17,29 @@ class Perfil(models.Model):
     endereco = models.CharField(max_length=200)
     telefone = models.CharField(max_length=15)
 
-    def str(self):
+    def __str__(self):
         return f"{self.nome} {self.sobrenome}"
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=200)
     imagem = models.CharField(max_length=200)
     autor = models.CharField(max_length=100)
-    preco = models.DecimalField(max_digits=6,decimal_places=2)
-    
+    preco = models.DecimalField(max_digits=6, decimal_places=2)
+    categoria = models.ForeignKey(
+        Categoria,
+        on_delete=models.CASCADE,
+        related_name='livros',
+        null=True,    # permite valor nulo no banco
+        blank=True    # permite formulário em branco
+    )
 
-    def str(self):
+    def __str__(self):
         return self.titulo
