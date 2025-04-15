0a00d62a4749e0217e5d601a140fe19989783056
JoaoGuilherme <jgbof@cesar.school>
2025-04-15 16:39:45 -0400

    up

--- meu_projeto/urls.py
+++ meu_projeto/urls.py
@@ -5,10 +5,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('forum.urls')),  # Inclui as rotas do app forum
+    path('', include('forum.urls')),
 ]
 
-# SERVINDO ARQUIVOS DE MÍDIA E STATIC NO DEV
+# Somente em desenvolvimento
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
