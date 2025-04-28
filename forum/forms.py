733f043fde6cd9ff80233b6cfe47536d7778eef0
JoaoGuilherme <jgbof@cesar.school>
2025-04-28 15:37:07 -0400

    Refatoração do formulário de registro de usuários

--- forum/forms.py
+++ forum/forms.py
@@ -8,51 +8,19 @@ class RegistroUsuarioForm(UserCreationForm):
         ('Jurídica', 'Pessoa Jurídica'),
     ]
 
-    tipo_pessoa = forms.ChoiceField(
-        choices=TIPO_PESSOA_CHOICES,
-        widget=forms.Select(attrs={'class': 'form-control'}),
-        required=True,
-    )
+    tipo_pessoa = forms.ChoiceField(choices=TIPO_PESSOA_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    nome = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    sobrenome = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    data_nascimento = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}))
+    cpf = forms.CharField(max_length=14, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    cep = forms.CharField(max_length=9, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    endereco = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    telefone = forms.CharField(max_length=15, widget=forms.TextInput(attrs={'class': 'form-control'}))
 
-    email = forms.EmailField(
-        required=True,
-        widget=forms.EmailInput(attrs={'class': 'form-control'})
-    )
-    nome = forms.CharField(
-        max_length=100,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    sobrenome = forms.CharField(
-        max_length=100,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    data_nascimento = forms.DateField(
-        widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'})
-    )
-    cpf = forms.CharField(
-        max_length=14,
-        label='CPF',
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    cep = forms.CharField(
-        max_length=9,
-        label='CEP',
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    endereco = forms.CharField(
-        max_length=200,
-        label='Endereço',
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    telefone = forms.CharField(
-        max_length=15,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2',
-                  'tipo_pessoa', 'nome', 'sobrenome', 'data_nascimento',
-                  'cpf', 'cep', 'endereco', 'telefone']
+        fields = ['username', 'email', 'password1', 'password2', 'tipo_pessoa', 'nome', 'sobrenome', 'data_nascimento', 'cpf', 'cep', 'endereco', 'telefone']
         widgets = {
             'username': forms.TextInput(attrs={'class': 'form-control'}),
             'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
