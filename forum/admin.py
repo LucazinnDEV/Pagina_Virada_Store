4f0ae697cb333978017bde2970edf58654012c98
JoaoGuilherme <jgbof@cesar.school>
2025-04-28 16:38:50 -0400

    Customiza admin de Livro, Categoria, Perfil e adiciona Wishlist

--- forum/admin.py
+++ forum/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
-from .models import Livro, Categoria, Perfil
+from .models import Livro, Categoria, Perfil, Wishlist
 
-admin.site.register(Livro)
-admin.site.register(Categoria)
-admin.site.register(Perfil)
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'preco', 'recomendado')  # Adiciona coluna "Recomendado" no admin
+    list_filter = ('recomendado', 'categoria')  # Filtro lateral por recomendado e categoria
+    search_fields = ('titulo', 'autor')  # Caixa de busca no admin
+    list_editable = ('recomendado',)  # Permite editar "recomendado" direto da lista
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    search_fields = ('nome',)
+
+@admin.register(Perfil)
+class PerfilAdmin(admin.ModelAdmin):
+    search_fields = ('nome', 'sobrenome', 'cpf')
+
+@admin.register(Wishlist)
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = ('user', 'livro')
+    search_fields = ('user__username', 'livro__titulo')
