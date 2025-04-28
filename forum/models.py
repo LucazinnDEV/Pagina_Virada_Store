df31e37dd0f860e2fc2ea820a2add4fb2f802023
JoaoGuilherme <jgbof@cesar.school>
2025-04-28 15:36:36 -0400

    Criação do model Wishlist e limpeza de imports não utilizados

--- forum/models.py
+++ forum/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import FileExtensionValidator
 
 class Perfil(models.Model):
     TIPO_PESSOA_CHOICES = [
@@ -35,13 +34,17 @@ class Livro(models.Model):
     preco = models.DecimalField(max_digits=6, decimal_places=2)
     vendas = models.PositiveIntegerField(default=0)
     recomendado = models.BooleanField(default=False)
-    categoria = models.ForeignKey(
-        Categoria,
-        on_delete=models.CASCADE,
-        related_name='livros',
-        null=True,
-        blank=True
-    )
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='livros', null=True, blank=True)
 
     def __str__(self):
         return self.titulo
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'livro')
+
+    def __str__(self):
+        return f"{self.user.username} deseja {self.livro.titulo}"
