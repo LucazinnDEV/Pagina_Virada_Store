ad78546c34b6c21df131c25ba85069730a19e067
Hugo Diêgo <98442609+Mihaael@users.noreply.github.com>
2025-05-01 17:47:29 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,9 +5,16 @@ module.exports = defineConfig({
     baseUrl: 'http://127.0.0.1:8000',
     supportFile: false,
     specPattern: 'cypress/e2e/**/*.cy.js',
-    defaultCommandTimeout: 10000,
-    pageLoadTimeout: 60000,
+    defaultCommandTimeout: 20000, 
+    pageLoadTimeout: 120000,
+    retries: {
+      runMode: 2, 
+      openMode: 0,
+    },
+    viewportWidth: 1280,
+    viewportHeight: 720,
   },
   video: true,
   screenshotOnRunFailure: true,
+  trashAssetsBeforeRuns: true, 
 })
