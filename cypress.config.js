add3705f4de558cc9438a8aa5820456b451617f0
Hugo Diêgo <98442609+Mihaael@users.noreply.github.com>
2025-05-01 17:42:45 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,10 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://127.0.0.1:8000',
     supportFile: false,
-    defaultCommandTimeout: 10000
-  }
+    specPattern: 'cypress/e2e/**/*.cy.js',
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 60000,
+  },
+  video: true,
+  screenshotOnRunFailure: true,
 })
