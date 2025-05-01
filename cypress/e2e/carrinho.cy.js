d8b64ef19643ec1e4ab6a86f275d9b4323cc4cb3
Rafaellc-DEV <rafaelcosta.2600@gmail.com>
2025-05-01 13:21:18 -0300

    update tentando corrigir erro no test

--- cypress/e2e/carrinho.cy.js
+++ cypress/e2e/carrinho.cy.js
@@ -3,8 +3,8 @@ describe('Carrinho', () => {
     cy.visit('/');
     cy.get('input[name="q"]').clear().type('pai');
     cy.get('form').first().submit();
-    cy.contains('Ver Detalhes').first().click();
-    cy.contains('COMPRAR AGORA').click();
+    cy.contains('Ver Detalhes', { timeout: 10000 }).first().click();
+    cy.contains('COMPRAR AGORA', { matchCase: false }).click();
     cy.url().should('include', '/carrinho/');
   });
 
