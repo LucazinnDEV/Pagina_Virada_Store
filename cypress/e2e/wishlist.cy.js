7753a15e023347042416e3d477f7897ad8c9aa1a
Hugo Diêgo <98442609+Mihaael@users.noreply.github.com>
2025-05-01 18:24:05 -0300

    Update wishlist.cy.js

--- cypress/e2e/wishlist.cy.js
+++ cypress/e2e/wishlist.cy.js
@@ -1,45 +1,17 @@
 describe('Lista de Desejos', () => {
-  
-  const login = (username, password) => {
-    cy.get('a[href="/login/"] i.bi-person-circle').click();
-    cy.url().should('include', '/login');
-    cy.contains('Faça login');
-
-    cy.get('input[name="username"]').type(username);
-    cy.get('input[name="password"]').type(password);
-    cy.contains('Entrar').click();
-    cy.url().should('eq', 'http://127.0.0.1:8000/');
-  };
+  beforeEach(() => {
+    cy.login('joao', '270406'); 
+  });
 
   it('Adiciona e remove da lista de desejos com usuário logado', () => {
-    cy.visit('/');
-    login('joao', '270406');
-
-    cy.get('input[name="q"]').type('pai');
-    cy.get('form').first().submit();
-    cy.url().should('include', '/buscar');
-
-    cy.contains('Ver Detalhes').click();
-    cy.url().should('match', /\/\d+\/$/);
-
-    cy.contains('Adicionar à Lista de Desejos').click();
-    cy.contains('Remover da Lista de Desejos').should('exist');
-
-    cy.contains('Remover da Lista de Desejos').click();
-    cy.contains('Adicionar à Lista de Desejos').should('exist');
+    cy.visit('/wishlist/');
+    cy.contains('Adicionar à lista de desejos').click();
+    cy.contains('Remover da lista de desejos').click();
   });
 
   it('Redireciona para login ao tentar acessar página de wishlist sem login', () => {
-    cy.visit('/');
-    cy.get('input[name="q"]').type('pai');
-    cy.get('form').first().submit();
-    cy.url().should('include', '/buscar');
-
-    cy.contains('Ver Detalhes').click();
-    cy.url().should('match', /\/\d+\/$/);
-
+    cy.logout(); 
     cy.visit('/wishlist/');
-    cy.url().should('include', '/login');
-    cy.contains('Faça login').should('exist');
+    cy.url().should('include', '/login/');
   });
 });
