c36b9abdef50e6aa2470aa3c9d7c2d3cb581c9dd
Hugo Diêgo <98442609+Mihaael@users.noreply.github.com>
2025-05-01 18:21:28 -0300

    Update checkout.cy.js

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,48 +1,33 @@
 describe('Finalizar Compra', () => {
   it('Compra com dados válidos', () => {
-    cy.visit('/');
-    cy.get('input[name="q"]').clear().type('pai');
-    cy.get('form').first().submit();
+    cy.visit('/checkout'); 
 
-    cy.contains('Ver Detalhes', { timeout: 10000 }).first().click();
-    cy.contains('COMPRAR AGORA', { timeout: 10000 }).click();
+    cy.get('input[name="endereco"]').type('Rua ABC, 123');
+    cy.get('input[name="cidade"]').type('São Paulo');
+    cy.get('input[name="cep"]').type('01000-000');
+    cy.get('input[name="numero_cartao"]').type('1234567812345678');
+    cy.get('input[name="data_validade"]').type('12/25');
+    cy.get('input[name="cvv"]').type('123');
 
-    cy.contains('Finalizar Compra', { timeout: 10000 }).click();
-    cy.url().should('include', '/confirmar-finalizacao/');
-    cy.contains('Sim, Finalizar Compra', { timeout: 10000 }).click();
-    cy.url().should('include', '/checkout/');
+    cy.contains('Finalizar Compra').click();
 
-    cy.get('input[name="nome"]').clear().type('João');
-    cy.get('input[name="sobrenome"]').clear().type('Cliente');
-    cy.get('input[name="cep"]').clear().type('50720000');
-    cy.get('input[name="endereco"]').clear().type('Rua das Letras, 123');
+    cy.wait(2000); 
 
-    cy.contains('Confirmar Compra', { timeout: 10000 }).click();
-
-    cy.url().should('include', '/checkout/');
-    cy.contains('Obrigado pela sua compra', { timeout: 10000 }).should('exist');
+    cy.contains('Ver Detalhes', { timeout: 15000 }).should('be.visible');
   });
 
   it('Tenta finalizar deixando um campo vazio', () => {
-    cy.visit('/');
-    cy.get('input[name="q"]').clear().type('pai');
-    cy.get('form').first().submit();
+    cy.visit('/checkout');
 
-    cy.contains('Ver Detalhes', { timeout: 10000 }).first().click();
-    cy.contains('COMPRAR AGORA', { timeout: 10000 }).click();
+    cy.get('input[name="endereco"]').type('Rua ABC, 123');
+    cy.get('input[name="cidade"]').type('');
+    cy.get('input[name="cep"]').type('01000-000');
+    cy.get('input[name="numero_cartao"]').type('1234567812345678');
+    cy.get('input[name="data_validade"]').type('12/25');
+    cy.get('input[name="cvv"]').type('123');
 
-    cy.contains('Finalizar Compra', { timeout: 10000 }).click();
-    cy.url().should('include', '/confirmar-finalizacao/');
-    cy.contains('Sim, Finalizar Compra', { timeout: 10000 }).click();
-    cy.url().should('include', '/checkout/');
+    cy.contains('Finalizar Compra').click();
 
-    cy.get('input[name="sobrenome"]').clear().type('Cliente');
-    cy.get('input[name="cep"]').clear().type('50720000');
-    cy.get('input[name="endereco"]').clear().type('Rua das Letras, 123');
-
-    cy.contains('Confirmar Compra', { timeout: 10000 }).click();
-
-    cy.url().should('include', '/checkout/');
-    cy.contains('Campo obrigatório', { timeout: 10000 }).should('exist'); 
+    cy.contains('Este campo é obrigatório').should('be.visible');
   });
 });
