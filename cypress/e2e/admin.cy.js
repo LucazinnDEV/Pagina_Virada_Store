d8328003f40b1ebcd65d50fe1c119ca941572a73
Hugo Diêgo <98442609+Mihaael@users.noreply.github.com>
2025-05-30 20:36:49 -0300

    Update admin.cy.js

--- cypress/e2e/admin.cy.js
+++ cypress/e2e/admin.cy.js
@@ -5,7 +5,10 @@ describe('Login Admin', () => {
     cy.get('#id_password').type('270406');
     cy.get('form').submit();
     cy.url().should('include', '/admin/');
-    cy.get('#site-name', { timeout: 10000 }).should('contain.text', 'Site administration');
+    cy.get('#site-name', { timeout: 10000 }).should(($el) => {
+      const text = $el.text();
+      expect(text).to.satisfy(t => t.includes('Administração') || t.includes('Site administration'));
+    });
   });
 
   it('Login com credenciais erradas', () => {
@@ -13,7 +16,13 @@ describe('Login Admin', () => {
     cy.get('#id_username').type('joao');
     cy.get('#id_password').type('senhaerrada');
     cy.get('form').submit();
-    cy.get('.errornote', { timeout: 10000 }).should('be.visible');
-    cy.get('.errornote').should('contain.text', 'Please enter the correct username and password');
+    cy.get('.errornote', { timeout: 20000 }).should('be.visible');
+    cy.get('.errornote').should(($el) => {
+      const text = $el.text();
+      expect(text).to.satisfy(t =>
+        t.includes('Por favor, insira um usuário e senha corretos') ||
+        t.includes('Please enter the correct username and password')
+      );
+    });
   });
 });
