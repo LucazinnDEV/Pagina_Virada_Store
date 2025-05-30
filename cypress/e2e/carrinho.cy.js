8fad46296956a8c2e7693d142ce857e9cc8bf4bb
Hugo Diêgo <98442609+Mihaael@users.noreply.github.com>
2025-05-30 20:43:17 -0300

    Update carrinho.cy.js

--- cypress/e2e/carrinho.cy.js
+++ cypress/e2e/carrinho.cy.js
@@ -1,27 +1,20 @@
 describe('Carrinho', () => {
   it('Adiciona item ao carrinho e acessa a página', () => {
     cy.visit('/');
-
-    // Comentado: Erro ocorre porque o botão "Adicionar ao Carrinho" não está sendo encontrado
-    // cy.contains('Adicionar ao Carrinho').click(); 
-
-    cy.wait(2000); // Aguardar carregamento da página, talvez ajude a encontrar o botão
-
-    // Comentado: Erro ocorre porque o link "Ver Carrinho" não está sendo encontrado
-    // cy.contains('Ver Carrinho').click();
-
-    // Comentado: A URL não está carregando como esperado após o clique
-    // cy.url().should('include', '/carrinho');
-
-    // Comentado: validação baseada em conteúdo da página, que está falhando no teste
-    // cy.contains('Ver Detalhes', { timeout: 15000 }).should('be.visible'); 
-    // cy.get('.carrinho-item').should('have.length.greaterThan', 0); 
+    cy.get('.produto', { timeout: 10000 }).first().within(() => {
+      cy.contains('Adicionar ao Carrinho', { timeout: 10000 }).click();
+    });
+    cy.wait(1000);
+    cy.contains('Ver Carrinho', { timeout: 10000 }).click();
+    cy.url().should('include', '/carrinho');
+    cy.get('.carrinho-item', { timeout: 10000 }).should('have.length.greaterThan', 0);
   });
 
   it('Tenta acessar o carrinho sem itens', () => {
+    cy.visit('/logout'); 
+    cy.clearCookies();
+    cy.clearLocalStorage();
     cy.visit('/carrinho');
-
-    // Comentado: validação baseada em conteúdo da página
-    // cy.contains('Seu carrinho está vazio').should('be.visible');
+    cy.contains('Seu carrinho está vazio', { timeout: 10000 }).should('be.visible');
   });
 });
