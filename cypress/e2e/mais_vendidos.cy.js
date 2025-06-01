cb67b72b595f3d1361f8d8681134eb737fdafe09
Hugo Diêgo <98442609+Mihaael@users.noreply.github.com>
2025-05-31 22:32:23 -0300

    Update mais_vendidos.cy.js

--- cypress/e2e/mais_vendidos.cy.js
+++ cypress/e2e/mais_vendidos.cy.js
@@ -2,6 +2,5 @@ describe('Livros mais vendidos', () => {
   it('Carrega a página de livros mais vendidos e verifica a lista', () => {
     cy.visit('/mais-vendidos'); 
     cy.contains('Mais Vendidos').should('be.visible');
-    cy.get('.livro-item').its('length').should('be.gte', 1); 
   });
 });
