43f7e7f796dbdaaebe2d04077ffaf0fa9828eb79
Mihaael <hugodiego.898@gmail.com>
2025-05-31 22:13:36 -0300

    Teste de mais vendidos

---
+++ cypress/e2e/mais_vendidos.cy.js
@@ -0,0 +1,7 @@
+describe('Livros mais vendidos', () => {
+  it('Carrega a página de livros mais vendidos e verifica a lista', () => {
+    cy.visit('/livros/mais-vendidos');
+    cy.contains('Mais Vendidos').should('be.visible');
+    cy.get('.livro-item').its('length').should('be.gte', 1); 
+  });
+});
