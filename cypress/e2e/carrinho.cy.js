942ced524d584912a30e6e43bc80e565a7fb7140
Hugo Diêgo <98442609+Mihaael@users.noreply.github.com>
2025-05-31 21:47:40 -0300

    Update carrinho.cy.js

--- cypress/e2e/carrinho.cy.js
+++ cypress/e2e/carrinho.cy.js
@@ -5,9 +5,9 @@ describe('Carrinho', () => {
   });
 
   it('Adiciona item ao carrinho e acessa a página do carrinho', () => {
-    cy.visit('/1/'); 
-    cy.get('#comprarBtn').click();
-    cy.get('#confirmModal').should('be.visible');
+    cy.visit('/1/');
+    cy.get('#comprarBtn').should('be.visible').click();
+    cy.get('#confirmModal', { timeout: 10000 }).should('be.visible');
     cy.get('#goToCart').click();
     cy.url().should('include', '/carrinho');
     cy.get('tbody tr').should('have.length.greaterThan', 0);
