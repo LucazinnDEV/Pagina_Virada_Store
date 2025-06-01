126fc09025ad5c0f0284d7304bc8ab78e7c6f3be
Hugo Diêgo <98442609+Mihaael@users.noreply.github.com>
2025-05-31 21:33:03 -0300

    Update carrinho.cy.js

--- cypress/e2e/carrinho.cy.js
+++ cypress/e2e/carrinho.cy.js
@@ -2,19 +2,22 @@ describe('Carrinho', () => {
   beforeEach(() => {
     cy.clearLocalStorage();
     cy.clearCookies();
+    cy.visit('/');
   });
 
   it('Adiciona item ao carrinho e acessa a página do carrinho', () => {
-    // cy.visit('/produto/1/');  // Depende do produto existir
-    // cy.get('#comprarBtn').click();  // Depende da página carregada
-    // cy.get('#confirmModal').should('be.visible');  // Depende do modal aparecer
-    // cy.get('#goToCart').click();  // Depende do modal
-    // cy.url().should('include', '/carrinho');  // Verifica url após clicar
-    // cy.get('tbody tr').should('have.length.greaterThan', 0);  // Verifica itens no carrinho
-    // cy.contains('Finalizar Compra').should('be.visible');  // Verifica botão visível
+    cy.visit('/produto/1/');
+    cy.get('#comprarBtn').click();
+    cy.get('#confirmModal').should('be.visible');
+    cy.get('#goToCart').click();
+    cy.url().should('include', '/carrinho');
+    cy.get('tbody tr').should('have.length.greaterThan', 0);
+    cy.contains('Finalizar Compra').should('be.visible');
   });
 
   it('Exibe mensagem ao acessar o carrinho vazio', () => {
+    cy.clearLocalStorage();
+    cy.clearCookies();
     cy.visit('/carrinho');
     cy.contains('Seu carrinho está vazio').should('be.visible');
   });
